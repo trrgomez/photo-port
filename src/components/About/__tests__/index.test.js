61fc29baee818d97eb084a0defd980513a4d116a
trgomez <88842062+trrgomez@users.noreply.github.com>
2022-03-14 12:28:42 -0500

    added nav testing

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -3,6 +3,8 @@ import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import About from "..";
 
+afterEach(cleanup);
+
 describe("About component", () => {
   // First Test
   it('renders', () => {
